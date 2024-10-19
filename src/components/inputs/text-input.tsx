8ae2424c448168caf49d57a26831265f37307686
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-10-20 00:01:40 +0800

    ✨ feat: Create custom text input

---
+++ src/components/inputs/text-input.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import React from 'react';
+import { TextInput } from 'react-native';
+
+export default function CustomTextInput(
+  props: React.ComponentProps<typeof TextInput>,
+) {
+  return (
+    <TextInput
+      {...props}
+      className={clsx(
+        `px-4 py-2 text-base w-full 
+        ${props.multiline ?? 'h-[58px]'}
+        ${
+          !props.className?.includes('border') &&
+          'border-2 border-gray-400 rounded-[20px]'
+        } text-white`,
+        props.className,
+      )}
+    />
+  );
+}
