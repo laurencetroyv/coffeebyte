a5ae5c5af1ebc9530b7c5cbee70b69a359015a6e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-10-24 19:29:47 +0800

    ✨ feat: Create props

---
+++ src/types/diagnosis_prop.ts
@@ -0,0 +1,6 @@
+export interface DiagnosisProps {
+  image: string;
+  name: string;
+  id: string;
+  createdAt: Date;
+}
---
+++ src/types/user_type.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  dateOfBirth: string;
+  session: string;
+  avatar: string;
+}
