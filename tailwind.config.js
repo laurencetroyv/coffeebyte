1c266342e6f3d22783b62bd2e7e211fa7f017722
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-10-24 19:31:11 +0800

    🔧 config: Ectend colors to have primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.js', 'App.tsx', 'src/**.{ts,tsx}'],
+  content: ['index.js', 'App.tsx', 'src/**/*.{ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3E735B',
+      },
+    },
   },
   plugins: [],
 };
